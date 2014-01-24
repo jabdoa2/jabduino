90a399ad80487ec53a4201e4bc7883a5a36c8b3c
jabdoa2 <jan-github@kantert.net>
2014-01-24 23:52:50 +0100

    update jabduino to arduino 1.0.5. fixes some compiler problems for newer gcc versions

--- cores/arduino/IPAddress.h
+++ cores/arduino/IPAddress.h
@@ -32,12 +32,16 @@
 
 class IPAddress : public Printable {
 private:
-    uint8_t _address[4];  // IPv4 address
+	union {
+		uint8_t a8[4];  // IPv4 address
+		uint32_t a32;
+	} _address;
+	
     // Access the raw byte array containing the address.  Because this returns a pointer
     // to the internal structure rather than a copy of the address this function should only
     // be used when you know that the usage of the returned uint8_t* will be transient and not
     // stored.
-    uint8_t* raw_address() { return _address; };
+    uint8_t* raw_address() { return _address.a8; }
 
 public:
     // Constructors
@@ -48,13 +52,13 @@ public:
 
     // Overloaded cast operator to allow IPAddress objects to be used where a pointer
     // to a four-byte uint8_t array is expected
-    operator uint32_t() { return *((uint32_t*)_address); };
-    bool operator==(const IPAddress& addr) { return (*((uint32_t*)_address)) == (*((uint32_t*)addr._address)); };
+    operator uint32_t() { return _address.a32; }
+    bool operator==(const IPAddress& addr) { return _address.a32 == addr._address.a32; };
     bool operator==(const uint8_t* addr);
 
     // Overloaded index operator to allow getting and setting individual octets of the address
-    uint8_t operator[](int index) const { return _address[index]; };
-    uint8_t& operator[](int index) { return _address[index]; };
+    uint8_t operator[](int index) const { return _address.a8[index]; };
+    uint8_t& operator[](int index) { return _address.a8[index]; };
 
     // Overloaded copy operators to allow initialisation of IPAddress objects from other types
     IPAddress& operator=(const uint8_t *address);
